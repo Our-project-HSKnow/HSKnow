b16e5006de3c0e005e6803bacd31391e7bf12afc
BLGS <13413419347@163.com>
2021-04-09 23:28:03 +0800

    NONE

--- app/src/main/java/com/example/hsknows/login/MainActivity.java
+++ app/src/main/java/com/example/hsknows/login/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.ViewAnimationUtils;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -33,31 +34,52 @@ public class MainActivity extends AppCompatActivity {
 
     Button button_login;
     Button button_register;
-    Button button_exit;
     Button close_button;
     FloatingActionButton button_tran_log;
     CardView login_card;
+    CardView register_card;
+    EditText register_name;
+    EditText register_account;
+    EditText register_password;
+    EditText editText_name;
+    EditText editText_pwd;
 
+    MotionLayout motionLayout;
+
+    TextView account;
+    TextView username;
+    TextView passward;
+    TextView log_username;
+    TextView log_passward;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_main);
 
-        Button close_button = (Button)findViewById(R.id.log_button_exit);
+        motionLayout = (MotionLayout)findViewById(R.id.log_motionlayout);
+
+        close_button = (Button)findViewById(R.id.log_button_exit);
 
         //登陆界面
-        final EditText editText_name=(EditText) findViewById(R.id.edittext_name);
-        final EditText editText_pwd=(EditText) findViewById(R.id.edittext_pwd);
+        editText_name=(EditText) findViewById(R.id.edittext_name);
+        editText_pwd=(EditText) findViewById(R.id.edittext_pwd);
         button_login = (Button) findViewById(R.id.button_login);
         button_tran_log = (FloatingActionButton) findViewById(R.id.button_tran_log);
-        button_exit = (Button) findViewById(R.id.log_button_exit);
         login_card = (CardView)findViewById(R.id.log_card);
+        log_username = (TextView)findViewById(R.id.log_username);
+        log_passward = (TextView)findViewById(R.id.log_passward);
 
         //注册界面
-        final EditText register_name = (EditText) findViewById(R.id.register_name);
-        final EditText register_account = (EditText) findViewById(R.id.register_account);
-        final EditText register_password = (EditText) findViewById(R.id.register_password);
+        register_card = (CardView)findViewById(R.id.register_card);
+        register_name = (EditText) findViewById(R.id.register_name);
+        register_account = (EditText) findViewById(R.id.register_account);
+        register_password = (EditText) findViewById(R.id.register_password);
         button_register = (Button) findViewById(R.id.button_registe);
+        username = (TextView)findViewById(R.id.username);
+        account = (TextView)findViewById(R.id.account);
+        passward = (TextView)findViewById(R.id.password);
+
+        ChangeVisibility(false);
 
 
         LitePal.getDatabase();
@@ -105,8 +127,84 @@ public class MainActivity extends AppCompatActivity {
                 user.setPassword(register_password.getText().toString());
                 user.save();
                 Toast.makeText(com.example.hsknows.login.MainActivity.this, "用户创建成功", Toast.LENGTH_SHORT).show();
+                motionLayout.transitionToStart();
             }
         });
+
+        motionLayout.setTransitionListener(new MotionLayout.TransitionListener() {
+            @Override
+            public void onTransitionStarted(MotionLayout motionLayout, int i, int i1) {
+                if (register_card.getAlpha() == 0)
+                {
+                    ChangeVisibility(true);
+                }
+                else if(register_card.getAlpha() == 1)
+                {
+                    ChangeVisibility(false);
+                }
+            }
+
+            @Override
+            public void onTransitionChange(MotionLayout motionLayout, int i, int i1, float v) {
+
+            }
+
+            @Override
+            public void onTransitionCompleted(MotionLayout motionLayout, int i) {
+                if (register_card.getAlpha() == 0)
+                {
+                    ChangeVisibility(false);
+                }
+                else if(register_card.getAlpha() == 1)
+                {
+                    ChangeVisibility(true);
+                }
+            }
+
+            @Override
+            public void onTransitionTrigger(MotionLayout motionLayout, int i, boolean b, float v) {
+
+            }
+        });
+    }
+    public void ChangeVisibility(boolean visible)
+    {
+        if (visible == true)
+        {
+            username.setVisibility(View.VISIBLE);
+            account.setVisibility(View.VISIBLE);
+            passward.setVisibility(View.VISIBLE);
+            register_card.setVisibility(View.VISIBLE);
+            register_name.setVisibility(View.VISIBLE);
+            register_account.setVisibility(View.VISIBLE);
+            register_password.setVisibility(View.VISIBLE);
+            button_register.setVisibility(View.VISIBLE);
+
+            login_card.setVisibility(View.GONE);
+            log_username.setVisibility(View.GONE);
+            log_passward.setVisibility(View.GONE);
+            button_login.setVisibility(View.GONE);
+            editText_name.setVisibility(View.GONE);
+            editText_pwd.setVisibility(View.GONE);
+        }
+        else
+        {
+            username.setVisibility(View.GONE);
+            account.setVisibility(View.GONE);
+            passward.setVisibility(View.GONE);
+            register_card.setVisibility(View.GONE);
+            register_name.setVisibility(View.GONE);
+            register_account.setVisibility(View.GONE);
+            register_password.setVisibility(View.GONE);
+            button_register.setVisibility(View.GONE);
+
+            login_card.setVisibility(View.VISIBLE);
+            log_username.setVisibility(View.VISIBLE);
+            log_passward.setVisibility(View.VISIBLE);
+            button_login.setVisibility(View.VISIBLE);
+            editText_name.setVisibility(View.VISIBLE);
+            editText_pwd.setVisibility(View.VISIBLE);
+        }
     }
 
 
