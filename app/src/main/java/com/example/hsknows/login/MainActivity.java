d2426096b897d9e6ea86fef58af57b261aedcd67
BLGS <13413419347@163.com>
2021-04-09 21:28:06 +0800

    UI界面--未定 Version ： 0.3 基本完成UI主界面 <!--TODO--> 工具箱界面 消息界面 menu菜单中进入各科目的界面 UI动效

--- app/src/main/java/com/example/hsknows/login/MainActivity.java
+++ app/src/main/java/com/example/hsknows/login/MainActivity.java
@@ -10,7 +10,6 @@ import android.view.ViewAnimationUtils;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -38,17 +37,6 @@ public class MainActivity extends AppCompatActivity {
     Button close_button;
     FloatingActionButton button_tran_log;
     CardView login_card;
-    CardView register_card;
-
-    EditText register_name;
-    EditText register_account;
-    EditText register_password;
-
-    TextView username;
-    TextView account;
-    TextView password;
-    TextView textView_username;
-    TextView textView_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,8 +44,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.login_main);
 
         Button close_button = (Button)findViewById(R.id.log_button_exit);
-        MotionLayout motionLayout = (MotionLayout)findViewById(R.id.log_motionlayout);
-        int status = 0;
 
         //登陆界面
         final EditText editText_name=(EditText) findViewById(R.id.edittext_name);
@@ -66,17 +52,12 @@ public class MainActivity extends AppCompatActivity {
         button_tran_log = (FloatingActionButton) findViewById(R.id.button_tran_log);
         button_exit = (Button) findViewById(R.id.log_button_exit);
         login_card = (CardView)findViewById(R.id.log_card);
-        textView_username = (TextView)findViewById(R.id.textView_username);
-        textView_password = (TextView)findViewById(R.id.textView_password);
+
         //注册界面
-        register_card = (CardView)findViewById(R.id.register_card);
-        register_name = (EditText) findViewById(R.id.register_name);
-        register_account = (EditText) findViewById(R.id.register_account);
-        register_password = (EditText) findViewById(R.id.register_password);
+        final EditText register_name = (EditText) findViewById(R.id.register_name);
+        final EditText register_account = (EditText) findViewById(R.id.register_account);
+        final EditText register_password = (EditText) findViewById(R.id.register_password);
         button_register = (Button) findViewById(R.id.button_registe);
-        username = (TextView)findViewById(R.id.username);
-        account = (TextView)findViewById(R.id.account);
-        password = (TextView)findViewById(R.id.password);
 
 
         LitePal.getDatabase();
@@ -124,71 +105,8 @@ public class MainActivity extends AppCompatActivity {
                 user.setPassword(register_password.getText().toString());
                 user.save();
                 Toast.makeText(com.example.hsknows.login.MainActivity.this, "用户创建成功", Toast.LENGTH_SHORT).show();
-                motionLayout.transitionToStart();
             }
         });
-
-        motionLayout.setTransitionListener(new MotionLayout.TransitionListener() {
-            @Override
-            public void onTransitionStarted(MotionLayout motionLayout, int i, int i1) {
-                if (register_card.getAlpha() == 0)
-                {
-                    ChangeVisible(true);
-                }
-                else if (register_card.getAlpha() == 1)
-                {
-                    ChangeVisible(false);
-                }
-            }
-
-            @Override
-            public void onTransitionChange(MotionLayout motionLayout, int i, int i1, float v) {
-
-            }
-
-            @Override
-            public void onTransitionCompleted(MotionLayout motionLayout, int i) {
-                if (register_card.getAlpha() == 0)
-                {
-                    ChangeVisible(false);
-                }
-                else if (register_card.getAlpha() == 1)
-                {
-                    ChangeVisible(true);
-                }
-            }
-
-            @Override
-            public void onTransitionTrigger(MotionLayout motionLayout, int i, boolean b, float v) {
-
-            }
-        });
-
-    }
-    public void ChangeVisible (boolean visible)
-    {
-        if (visible == true)
-        {
-            register_card.setVisibility(View.VISIBLE);
-            register_account.setVisibility(View.VISIBLE);
-            register_name.setVisibility(View.VISIBLE);
-            register_password.setVisibility(View.VISIBLE);
-            button_register.setVisibility(View.VISIBLE);
-            username.setVisibility(View.VISIBLE);
-            account.setVisibility(View.VISIBLE);
-            password.setVisibility(View.VISIBLE);
-        }
-        else
-        {
-            register_card.setVisibility(View.GONE);
-            register_account.setVisibility(View.GONE);
-            register_name.setVisibility(View.GONE);
-            register_password.setVisibility(View.GONE);
-            button_register.setVisibility(View.GONE);
-            username.setVisibility(View.GONE);
-            account.setVisibility(View.GONE);
-            password.setVisibility(View.GONE);
-        }
     }
 
 
